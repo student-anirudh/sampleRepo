0ae95a97419cb54d9434acca7237d4aac978bbda
student-anirudh <yourboss382@gmail.com>
2024-05-22 09:42:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Calculator from './Calculator'
+import './style.css'
+
+function App() {
+  return (
+    <div>
+   
+        <h1>Calculator</h1>
+      <Calculator></Calculator>
+      
+    </div>
+  )
+}
+
+export default App
